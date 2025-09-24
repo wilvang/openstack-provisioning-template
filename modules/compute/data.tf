fb403b076ffb467e68a49b939f25deb899cab5ad
Johan Fredrik Wilvang <johan@wilvang.com>
2025-09-24 11:54:41 +0200

    refactor(compute): use port_id for floating IP association instead of instance_id.

--- modules/compute/data.tf
+++ modules/compute/data.tf
@@ -2,26 +2,7 @@
 # OPENSTACK DATA SOURCES
 # ============================================
 # This section defines the data sources used to retrieve information about 
-# OpenStack resources such as images, key pairs, flavors, and networks.
-
-# --------------------------------------------
-# Instance Image Data Source
-# --------------------------------------------
-# Retrieves the most recent image by the specified name from OpenStack.
-# This image will be used to launch new instances.
-data "openstack_images_image_v2" "instance_image" {
-  name        = var.image_name
-  most_recent = true
-}
-
-# --------------------------------------------
-# Instance Flavor Data Source
-# --------------------------------------------
-# Retrieves the flavor for the instance using the specified flavor ID.
-# This determines the resources (CPU, RAM, etc.) available for the instance.
-data "openstack_compute_flavor_v2" "instance_flavor" {
-  flavor_id = var.flavor_id
-}
+# OpenStack resources such as key pairs, networks and ports.
 
 # --------------------------------------------
 # Key Pair Data Source
@@ -47,5 +28,16 @@ data "openstack_networking_network_v2" "ext_network" {
 # Retrieves the subnet IDs associated with the external network.
 # These subnets are used for connecting instances to external resources.
 data "openstack_networking_subnet_ids_v2" "ext_subnets" {
+  network_id = data.openstack_networking_network_v2.ext_network.id
+}
+
+
+# --------------------------------------------
+# Networking Port Data Source
+# --------------------------------------------
+data "openstack_networking_port_v2" "instance_port" {
+  for_each = { for vm in var.vm_setup : vm.name => vm }
+
+  device_id  = openstack_compute_instance_v2.vm_instance[each.key].id
   network_id = data.openstack_networking_network_v2.ext_network.id
 }
