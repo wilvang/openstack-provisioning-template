562807753e91f82f07f56270005ae9bd0be0a3e7
Johan Fredrik Wilvang <johan@wilvang.com>
2025-09-25 15:37:15 +0200

    refactor(compute): Remove redundant comma.

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -31,7 +31,7 @@ resource "openstack_compute_instance_v2" "vm_instance" {
   image_name      = var.image_name
   flavor_name     = var.flavor_name
   key_pair        = var.keypair_name
-  security_groups = [ openstack_networking_secgroup_v2.vm_secgroup[each.key].name, ]
+  security_groups = [ openstack_networking_secgroup_v2.vm_secgroup[each.key].name ]
 
   user_data = templatefile("${path.module}/${var.template[each.key]}", {})
 
