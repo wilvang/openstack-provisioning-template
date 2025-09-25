1b9875ce3432c8bc0286a8c410297b861d5db892
Johan Fredrik Wilvang <johan@wilvang.com>
2025-09-25 13:19:17 +0200

    feat(networking): Add outputs the subnet id.

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -7,9 +7,21 @@
 # like IP addresses, resource IDs, or names that may
 # need to be referenced later in other modules or by users.
 
-# This output will return the name of the network
-# created in the OpenStack environment.
-output "network_name" {
-  description = "The name of the network that was created in OpenStack."
-  value       = openstack_networking_network_v2.network.name
+# --------------------------------------------
+# Network ID Output
+# --------------------------------------------
+# Returns the ID of the OpenStack network resource created,
+# allowing other modules or users to reference this network.
+output "network_id" {
+  description = "The ID of the network created in OpenStack."
+  value       = openstack_networking_network_v2.network.id
+}
+
+# --------------------------------------------
+# Subnet IDs Output
+# --------------------------------------------
+# Returns a map of subnet keys to their corresponding subnet IDs.
+# This enables easy lookup of subnet IDs for subnets managed by this module.
+output "subnet_ids" {
+  value = { for key, subnet in openstack_networking_subnet_v2.subnet : key => subnet.id }
 }
